0003e3ae166039a86ae4ca38b7d5ac962a550468
yuki Ito <0yuki.alternative3@gmail.com>
2019-11-15 00:31:04 +0900

    publisher

--- publisher.js
+++ publisher.js
@@ -10,7 +10,16 @@ client.on("connect", function() {
 });
 
 setInterval(function() {
-  var message = Date.now().toString();
-  client.publish("topic0", message);
+  var message ="datetime:"+ Date.now().toString();
+  message+=",ax:"+0.1;
+  message+=",ay:"+0.2;
+  message+=",az:"+0.3;
+  message+=",lx:"+0.1;
+  message+=",ly:"+0.2;
+  message+=",lz:"+0.3;
+  message+=",gx:"+0.1;
+  message+=",gy:"+0.2;
+  message+=",gz:"+0.3;
+  client.publish("itoyuNineAxis", message);
   console.log("publisher.publish:", message);
 }, 1000);
